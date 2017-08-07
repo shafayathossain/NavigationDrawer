9da0fae2674b94d7a07c9ba304f8cd33c0f67f1e
Shafayat <shafayathossainkhan@gmail.com>
2017-08-08 04:38:20 +0600

    nevigation drawer added with main activity

--- app/src/main/java/com/example/shafayat/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/shafayat/navigationdrawer/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.shafayat.navigationdrawer;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.navigation_drawer);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
         setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+
+        toggle.syncState();
     }
 }
